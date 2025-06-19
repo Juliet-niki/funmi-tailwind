00f27a1188bca7e4a00f9a130db496ef24000b8c
Juliet-niki <nwajully@gmail.com>
2025-06-19 19:05:58 +0100

    installed angular material

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
